1477273d627d7db3e3f2db9022a66fd724857494
Raul <san21401@uvg.edu.gt>
2022-03-14 17:27:56 -0600

    Generacion de Stacks  y logica
    Se creo las clases respectivas para los stacks y se establecieron los metodos para la logica.

---
+++ StackAbstract.java
@@ -0,0 +1,13 @@
+/**
+ * StackAbstract
+ * Objetivo: Clase Abstracta de Stack 
+ * 
+ * @author Aaron Beltran
+ * @author Paulo Sánchez
+ * @author Luis Montenegro
+ * @author Roberto Rios
+ * @version 1.0 finalizado 14/03/2022
+ */
+public abstract class StackAbstract<E> implements StackInt<E>{
+    public StackAbstract(){}
+}
